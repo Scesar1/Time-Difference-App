dd0421748b9efc6661c969417de80c2a786a15e1
scesar1 <scesar1@jhu.edu>
2022-02-13 03:38:56 -0500

    initial commit

---
+++ app/src/main/java/com/example/timedifference/CalculateActivity.java
@@ -0,0 +1,125 @@
+package com.example.timedifference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CalculateActivity extends AppCompatActivity {
+    private final String[] time = {"AM", "PM"};
+    private SharedPreferences myPrefs;
+    private Integer state;
+    private EditText startHour, startMinute, endHour, endMinute;
+    private TextView diffHour, diffMinute;
+    private ImageButton calcButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate);
+        Intent intent = getIntent();
+
+        Context context = getApplicationContext();
+        myPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        state = myPrefs.getInt("toggleState", 1);
+
+        Spinner timeSelector1 = findViewById(R.id.timeSelector1);
+        timeSelector1.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, time));
+
+        Spinner timeSelector2 = findViewById(R.id.timeSelector2);
+        timeSelector2.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, time));
+        timeSelector2.setSelection(1);
+
+        startHour = findViewById(R.id.hourText);
+        startMinute = findViewById(R.id.minText);
+        endHour = findViewById(R.id.hourText2);
+        endMinute = findViewById(R.id.minText2);
+
+        calcButton = findViewById(R.id.hourglassCalc);
+
+
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int hour1 = Integer.parseInt(startHour.getText().toString());
+                int min1 = Integer.parseInt(startMinute.getText().toString());
+
+                int hour2 = Integer.parseInt(endHour.getText().toString());
+                int min2 = Integer.parseInt(endMinute.getText().toString());
+
+                if (hour1 > 24 || hour1 < 0) {
+                    startHour.setText("00");
+                    Toast.makeText(CalculateActivity.this, "INVALID NUMBER", Toast.LENGTH_SHORT).show();
+                }
+                if (hour2 > 24 || hour1 < 0) {
+                    endHour.setText("00");
+                    Toast.makeText(CalculateActivity.this, "INVALID NUMBER", Toast.LENGTH_SHORT).show();
+                }
+                if (min1 > 59 || min1 < 0) {
+                    startMinute.setText("00");
+                    Toast.makeText(CalculateActivity.this, "INVALID NUMBER", Toast.LENGTH_SHORT).show();
+                }
+                if (min2 > 59 || min2 < 0) {
+                    endMinute.setText("00");
+                    Toast.makeText(CalculateActivity.this, "INVALID NUMBER", Toast.LENGTH_SHORT).show();
+                }
+
+                getTimeDifference();
+            }
+        });
+
+
+        if (state == 1) {
+            timeSelector1.setVisibility(View.GONE);
+            timeSelector2.setVisibility(View.GONE);
+        }
+    }
+
+    public void getTimeDifference() {
+        startHour = findViewById(R.id.hourText);
+        startMinute = findViewById(R.id.minText);
+        endHour = findViewById(R.id.hourText2);
+        endMinute = findViewById(R.id.minText2);
+
+        int hour1 = Integer.parseInt(startHour.getText().toString());
+        int min1 = Integer.parseInt(startMinute.getText().toString());
+
+        int hour2 = Integer.parseInt(endHour.getText().toString());
+        int min2 = Integer.parseInt(endMinute.getText().toString());
+
+        Integer minTotal1 = hour1 * 60 + min1;
+        Integer minTotal2 = hour2 * 60 + min2;
+
+        Integer hourResult = Math.abs(minTotal1 - minTotal2) / 60;
+        Integer minuteResult = Math.abs(minTotal1 - minTotal2) % 60;
+
+
+
+
+        diffHour = findViewById(R.id.hourText3);
+        diffMinute = findViewById(R.id.minText3);
+
+        String hourText = String.format("%02d", hourResult);
+        String minuteText = String.format("%02d", minuteResult);
+
+        diffHour.setText(hourText);
+        diffMinute.setText(minuteText);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/timedifference/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.timedifference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+    private SharedPreferences myPrefs;
+    private Integer state;
+    private RadioGroup rgClockMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Context context = getApplicationContext();
+        myPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        SharedPreferences.Editor peditor = myPrefs.edit();
+        peditor.putInt("toggleState", 1);
+        peditor.apply();
+
+        Button calculateButton = findViewById(R.id.calcButton);
+        rgClockMode = findViewById(R.id.toggle);
+
+        rgClockMode.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case (R.id.military):
+                        state = 1;
+                        break;
+                    case (R.id.standard):
+                        state = 0;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        calculateButton.setOnClickListener(view -> startCalculateActivity());
+    }
+
+    public void startCalculateActivity() {
+        Intent intent = new Intent(this, CalculateActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        state = myPrefs.getInt("toggleState", 1);
+        rgClockMode = findViewById(R.id.toggle);
+        if (state == 1) {
+            rgClockMode.check(R.id.military);
+        } else {
+            rgClockMode.check(R.id.standard);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        SharedPreferences.Editor peditor = myPrefs.edit();
+        peditor.putInt("toggleState", state);
+        peditor.apply();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        SharedPreferences.Editor peditor = myPrefs.edit();
+        peditor.putInt("toggleState", state);
+        peditor.apply();
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        SharedPreferences.Editor peditor = myPrefs.edit();
+        peditor.putInt("toggleState", state);
+        peditor.apply();
+
+        super.onDestroy();
+    }
+}
